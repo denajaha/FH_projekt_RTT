ed06f12f96a55496557db678ea3345129aaf6bd0
tinko <obi.martinko@gmail.com>
2022-01-06 19:51:19 +0100

    Updated JSON File with correct IDs and a full List of grocerys. Also added functions to read dynamic JSON Files without knowing the Keywords

--- src/main/java/com/projekt/Items.java
+++ src/main/java/com/projekt/Items.java
@@ -1,54 +1,79 @@
 package com.projekt;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
-
 public class Items {
 
-    private String[] items = new String[999999];
-
-    public static void readItemfromJson() {
-            JSONParser parser = new JSONParser();
-
-            try{
-                Object obj = parser.parse(new FileReader("C:\\Users\\tinko\\IdeaProjects\\FH_projekt_RTT\\src\\main\\java\\com\\projekt\\Supermarkt.json"));
-
-                JSONObject jsonObject = (JSONObject) obj;
-
-                String[] array = new String[10];
-
-                //array = getKeyWordfromJson(jsonObject);
-
-                JSONArray Lebensmittel = (JSONArray) jsonObject.get("Gemüse");
-                Iterator<String> iterator = Lebensmittel.iterator();
-                while (iterator.hasNext()){
-                    System.out.println(iterator.next());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
+    public static void loadDataFromJson() {
+        StringBuilder string = new StringBuilder();
+        try {
+            FileReader fileReader = new FileReader("Supermarkt.json");
+            int i;
+            while ((i = fileReader.read()) != -1) {
+                //System.out.print((char) i);
+                string.append((char) i);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String JSONstring = string.toString();
+
+        // Muss so erweitert werden damit der String dynamisch wird
+        int counter=0;
+        String[] price = new String[100];
+        String[] keyword = new String[100];
+
+        //Dynamische Funktion um die Keywords herauszubekommen falls man diese nicht kennt aus der JSON Datei
+        JSONObject jsonObj= new org.json.JSONObject(JSONstring);
+        Iterator<String> keys = jsonObj.keySet().iterator();
+        while (keys.hasNext()){
+            keyword[counter] = keys.next();
+            counter++;
+        }
+        JSONArray jsonArr = jsonObj.getJSONArray(keyword[0]);
+        String[] Test = new String[100];
+
+        //Dynamische Funktion um die SubKeywords aus dem Array herauszubekommen falls man diese nicht kennt
+        int test =0;
+        JSONObject jsonObs = jsonArr.getJSONObject(test);
+        Iterator<String > penis = jsonObs.keySet().iterator();
+        while(penis.hasNext()) {
+            Test[test] = penis.next();
+            test ++;
+        }
+
+        // Muss erweitert werden um Sub-Keys zu bekommen ohne diese zu wissen um dieses universel zu machen
+        for(int i=0; i<5; i++){
+            //JSONObject jsonOb = jsonArr.getJSONObject(i);
+            price[i] = jsonObs.getString(Test[i]);
+            //keyword[0] = jsonOb.getString("Konserven");
+            //keyword[1] = jsonOb.getString("Essig");
+        }
+
+        float[] parser = new float[100];
+
+        // muss verbessert werden um die Preise von Strings in Floating Zahlen umzuwandeln
+        for(int i =0; i<5;i++) {
+            parser[i] = Float.parseFloat(price[i]);
+            System.out.print(Test[i]);
+            System.out.print(" ");
+            System.out.println(parser[i]);
+        }
+
+
+
     }
-
-    public static String[] getKeyWordfromJson (JSONObject jsonObject){ //Hiermit kann man die Keywords der Json Datei erhalten
-        int counter = 0;
-        Iterator<String> keys = jsonObject.keySet().iterator();
-        //To count the length of the Array
-        while(keys.hasNext()) counter++;
-        String[] keyword = new String[counter];
-
-        return keyword;
-    }
-
     public static void main(String[] args) {
-        readItemfromJson();
+        loadDataFromJson();
     }
+
+
 
 
 }
