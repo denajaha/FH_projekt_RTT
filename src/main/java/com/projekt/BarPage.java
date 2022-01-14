c888881a96a107943e7fb7db4c77a2a55fb67823
tinko <obi.martinko@gmail.com>
2022-01-14 18:47:05 +0100

    Finished MainPage with comments and also BarPage with comments

--- src/main/java/com/projekt/BarPage.java
+++ src/main/java/com/projekt/BarPage.java
@@ -1,28 +1,35 @@
 package com.projekt;
-
-import java.time.Period;
 import java.util.*;
-import java.util.logging.Handler;
 
 public class BarPage {
 
-    private final ArrayList<String> listofchoosenitems = new ArrayList<String>();
-    private final ArrayList<String> order = new ArrayList<String>();
-    private final ArrayList<Integer> orderamount = new ArrayList<Integer>();
+    // Variable declaration
+    private final ArrayList<String> listofallselecteditems = new ArrayList<>();
+    private final Map<String, Float> finalorder = new TreeMap<>();
+    private final ArrayList<Integer> clicks = new ArrayList<>();
+    private final ArrayList<String> items = new ArrayList<>();
+    private final ArrayList<Float> price = new ArrayList<>();
+    private final Map<String, Integer> mp = new TreeMap<>();
 
+    private float total = 0;
 
-    public ArrayList<String> getlistofchoosenitems() { return listofchoosenitems;}
-    public ArrayList<String> getOrder() { return order; }
-    public ArrayList<Integer> getOrderamount() { return orderamount; }
+    /* Methods to be able to get the values Clicks/Total/finalorder etc --> They are Arraylist/Maps.
+       Implement ArrayLists<String> or Map<String, Integer>
+    */
+    public float getTotal() { return total; }
+    public ArrayList<Integer> getClicks() { return clicks; }
+    public Map<String, Float> getFinalorder() { return finalorder; }
+    public ArrayList<String> getlistofallselecteditems() { return listofallselecteditems;}
 
+    // Public BarPage to be able to use the Methods --> Just implement BarPage bar = new Barpage()
     public BarPage() {}
 
+    // Method to sort our duplicated clicked purchasing
     public void orderlist(ArrayList<String> list) {
-        // We need to creat a Map to load our list inside to double check how often an Item is in our List
-        Map<String, Integer> mp = new TreeMap<>();
-
-        // Loop to get our Subkeys from our list
-        for (int i = 0; i < listofchoosenitems.size(); i++) {
+        /* We need to create a Map to load our list inside to double check how often an item is in our List
+           Loop to get our Subkeys from our list
+        */
+        for (int i = 0; i < listofallselecteditems.size(); i++) {
             // Checking if our Subkey is in our list and adding how often it was found
             if(mp.containsKey((list.get(i)))){
                 mp.put(list.get(i), mp.get(list.get(i))+1);
@@ -30,16 +37,17 @@ public class BarPage {
             else mp.put(list.get(i),1);
         }
 
-        // Loop to get our Values and Keys in our Public list to get excess
+        /* Loop to get our Values how often a Product was clicked/chosen for every Product one separate List entry
+           items.add --> is a list with all chosen non duplicated products.
+        */
         for(Map.Entry<String, Integer> entry: mp.entrySet()){
-            order.add(entry.getKey());
-            orderamount.add(entry.getValue());
+            items.add(entry.getKey());
+            clicks.add(entry.getValue());
         }
     }
 
-
-/*
-    public void deleteItem(mainPage.getKey<mainPage.getSubKey()> listeDerGewähltenItems) { //Hier habe ich Von den Überkathegorien den Unterkathegorien dies Liste der gewählten Items eingegrenzt
+// These are features to add or remove an item to the order list
+/* public void deleteItem(mainPage.getKey<mainPage.getSubKey()> listeDerGewähltenItems) { //Hier habe ich Von den Überkathegorien den Unterkathegorien dies Liste der gewählten Items eingegrenzt
         System.out.println("Wählen Sie das gewählte Produkt, welches Sie entfernen wollen: ");
         Scanner loeschen = new Scanner(System.in);
         String item = loeschen.nextLine();
@@ -66,81 +74,125 @@ public class BarPage {
                 anzahl ++; //wieder anzahl von order
                 return;
             }
-
         }
+    }*/
 
-
-
-
+    // print Method to get the amount of clicked items and price per clicks.
+    public void printorder (){
+        for (int i = 0; i < clicks.size(); i++) {
+            System.out.println(clicks.get(i)+ "x " + items.get(i) + " " + price.get(i) + "€");
+        }
     }
 
- */
+    // The total price --> From finalorder: Sum of clicks * price
+    public void total() {
 
-    public void total(String[] price) {
-        float gesammt = 0;
-        for (int i = 0; i < 7; i++) {
-            gesammt += Float.parseFloat(price[i]);
+        float[] multiplier = new float[clicks.size()];
+
+        // Loop to get our price associated to the Order we choose
+        for (Map.Entry<String, Float> entry: finalorder.entrySet()) {
+            price.add(entry.getValue());
         }
-        System.out.print("Der Gesammtpreis beträgt: ");
-        System.out.print(gesammt);
-
+            for (int i = 0; i < finalorder.size(); i++) {
+               multiplier[i] = price.get(i) * clicks.get(i);
+               total += multiplier[i];
+            }
     }
 
 
     public static void main(String[] args) {
-
+        // Created Objects --> MainPag and BarPage
         MainPage main = new MainPage();
         BarPage bar = new BarPage();
-        String[] s = new String[50];
+
         Scanner scanner = new Scanner(System.in);
-        int choice = 0;
+
+        // Variables to help us to choice between MainKey and SubKey
+        int ChoiceMainKey;
+        int ChoiceSubKey;
+
+        // ArrayList to help us to store our Keys, Subkeys and Prices
         ArrayList<String> keys;
         ArrayList<String> subkeys;
-        ArrayList<Integer> test = new ArrayList<Integer>();
-        int x = 0;
+        ArrayList<String> price;
 
+        // Loading our Json File from Destiny
         main.loadDataFromJson("Supermarkt.json");
+
+        // Loading our MainKeys from Json
         main.getKeyFromJson(main.getJSONstring());
 
 
+        /* A Simple For-Loop to simulated clicks and a grocery store. Variable attempt how often we can choose
+           between two MainCategories
+        */
+        for (int attempt  = 0; attempt < 3; attempt++) {
 
-        //main.getKeyFromJson(main.getJSONstring());
-        // main.getsubKeyFromJson(main.getJSONstring(), "Lebensmittel");
-        //main.getPriceFromsubKeys(main.getJSONstring(), "Lebensmittel");
-        //s = main.getPrice();
-        System.out.println("Wähle die Kathegorie 1 bis 11: ");
-        main.printMainKey();
-        keys = main.getMainKey();
-        System.out.print("Auswahl: ");
-        choice = scanner.nextInt();
+            System.out.println("Wähle die Kategorie 1 bis 11: ");
+            main.printMainKey();
+            keys = main.getMainKey();
+            System.out.print("Auswahl: ");
+            ChoiceMainKey = scanner.nextInt();
 
-        choice -= 1;
+            // -1 to get the position from list because the list is starting with position 0 like an Array
+            ChoiceMainKey -= 1;
 
-        switch (choice) {
-            case 0:
-                main.getsubKeyFromJson(main.getJSONstring(), keys.get(0));
-                System.out.println();
-                main.printSubKey();
-                subkeys = main.getSubKey();
-                for (int i = 0; i < 5; i++) {
-                    System.out.print("Auswahl: ");
-                    x = scanner.nextInt();
-                    x -= 1;
-                    bar.listofchoosenitems.add(subkeys.get(x));
-                }
-                bar.orderlist(bar.listofchoosenitems);
-                for (int i = 0; i < bar.order.size(); i++) {
-                    System.out.println(bar.orderamount.get(i)+"x "+bar.order.get(i));
-                }
-                System.out.println();
-                break;
+            // Loading SubKeys from MainPage and also the Prices of the respective SubKey
+            main.getsubKeyFromJson(keys.get(ChoiceMainKey));
+            main.getPriceFromsubKeys( keys.get(ChoiceMainKey));
 
-            default:
-                System.out.println("Auswahl nicht verfügbar.");
-                break;
+            // Saving our Subkeys and Prices into a List to work with them in our Simulation
+            subkeys = main.getSubKey();
+            price = main.getPrice();
 
+            // Placeholder
+            System.out.println();
+
+            main.printSubKey();
+
+            // A simple For-Loop to simulate five products from same MainCategory in our Shopping venture
+            for (int i = 0; i < 5; i++) {
+
+                System.out.print("Auswahl: ");
+                ChoiceSubKey = scanner.nextInt();
+
+                // -1 to get the position from list because the list is starting with position 0 like an Array
+                ChoiceSubKey -= 1;
+
+                // We are adding all choices to our List --> listofallselecteditems (also duplicates)
+                bar.listofallselecteditems.add(subkeys.get(ChoiceSubKey));
+                /* We are adding to our choices the price so customer get an overview
+                   The Map is also need for the Total Methods to calculate the Total amount --> See public void Total()
+                 */
+                bar.finalorder.put(subkeys.get(ChoiceSubKey), Float.parseFloat(price.get(ChoiceSubKey)));
+            }
+
+            // Placeholder
+            System.out.println();
+
+            // !!! Important the Lists has to be cleared after a use !!!
+            main.clearSubKeylist();
+            main.clearPricelist();
+            main.ResetPriceCount();
+            main.ResetSubKeyCount();
+
+            // !!! Also our Variable/Lists for Simulating need to be cleared after using them !!!
+            subkeys.clear();
+            price.clear();
         }
 
+        // Method to eliminate duplicates but also to count them --> Simulating clicks
+        bar.orderlist(bar.listofallselecteditems);
+
+        // Method to calculate the total amount of price
+        bar.total();
+
+        // Placeholder
+        System.out.println();
+
+        bar.printorder();
+
+        System.out.println("Total: " + bar.getTotal() + "€");
 
     }
 }
--- src/main/java/com/projekt/MainPage.java
+++ src/main/java/com/projekt/MainPage.java
@@ -5,23 +5,20 @@ import org.json.JSONObject;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class MainPage {
 
     // Variable Declarations
-    private final ArrayList<String> MainKey = new ArrayList<String>();
-    private final ArrayList<String> SubKey = new ArrayList<String>();
-    private final ArrayList<String> Price = new ArrayList<String>();
+    private final ArrayList<String> MainKey = new ArrayList<>();
+    private final ArrayList<String> SubKey = new ArrayList<>();
+    private final ArrayList<String> Price = new ArrayList<>();
     private String JSONstring;
 
 
-    private static int KeyCount = 0;
-    private static int subKeyCount = 0;
-    private static int pricecount = 0;
+    private  int KeyCount = 0;
+    private  int subKeyCount = 0;
+    private  int pricecount = 0;
 
     // Public MainPage to be able to use my Methods --> Just implement MainPage main = new MainPage()
     public MainPage() {}
@@ -34,8 +31,11 @@ public class MainPage {
     public ArrayList<String> getMainKey() { return MainKey; }
     public ArrayList<String> getSubKey() { return SubKey; }
     public ArrayList<String> getPrice() { return Price; }
+    public int getKeyCount() { return KeyCount; }
+    public int getSubKeyCount() { return subKeyCount; }
+    public int getPricecount() { return pricecount; }
 
-    // It is Loading the whole JSON File and its saving into JSONstring --> Its needed to get Keys and SubKey etc.
+    // It is Loading the whole JSON File and its saving into JSONstring --> It's needed to get Keys and SubKey etc.
     public void loadDataFromJson(String FileName) {
         StringBuilder string = new StringBuilder();
         try {
@@ -50,12 +50,21 @@ public class MainPage {
         this.JSONstring = string.toString();
     }
 
+    public JSONObject creatJsonObject (String Key){
+        loadDataFromJson("Supermarkt.json");
+        JSONObject jsonObj = new org.json.JSONObject(getJSONstring());
+        JSONArray jsonArr = jsonObj.getJSONArray(Key);
+
+        return jsonArr.getJSONObject(0);
+    }
+
     // It gets the main keywords from our JSON file
     public void getKeyFromJson(String JSONstring) {
         JSONObject jsonObj = new org.json.JSONObject(JSONstring);
 
-        // It is a better version for while-Loop --> while(xyz.hasNext())
-        // The loop keep going through the JSON file till jsonObs.keySet() = xyz.hasNext() = NULL
+        /* It is a better version for while-Loop --> while(xyz.hasNext())
+           The loop keep going through the JSON file till jsonObs.keySet() = xyz.hasNext() = NULL
+        */
         for (String value : jsonObj.keySet()) {
             MainKey.add(value);
             KeyCount++;
@@ -64,61 +73,80 @@ public class MainPage {
     }
 
     // It gets the sub keywords from our JSON file
-    public void getsubKeyFromJson(String JSONstring, String Key) {
-        JSONObject jsonObj = new org.json.JSONObject(JSONstring);
-        JSONArray jsonArr = jsonObj.getJSONArray(Key);
-        JSONObject jsonObs = jsonArr.getJSONObject(subKeyCount);
+    public void getsubKeyFromJson(String Key) {
+        JSONObject jsonObs = creatJsonObject(Key);
 
-        // It is a better version for while-Loop --> while(xyz.hasNext())
-        // The loop keep going through the JSON file till jsonObs.keySet() = xyz.hasNext() = NULL
+        /* It is a better version for while-Loop --> while(xyz.hasNext())
+           The loop keep going through the JSON file till jsonObs.keySet() = xyz.hasNext() = NULL
+        */
         for (String value : jsonObs.keySet()) {
             SubKey.add(value);
             subKeyCount++;
         }
     }
 
-    //It gets the prices from our JSON file. But be carefull the Values are Strings. The reason that for is --> jsonObs.getString
-    public void getPriceFromsubKeys (String JSONstring, String Key){
+    /* It gets the prices from our JSON file.
+       But be carefull the Values are Strings. The reason that for is --> jsonObs.getString
+    */
+    public void getPriceFromsubKeys (String Key){
 
-        //We have to creat our JsonObject through the JSONstring. The JSONstring contains the whole Filedata.
-        JSONObject jsonObj = new org.json.JSONObject(JSONstring);
-        //Our Subkeys are inside JSONArrays. On top is our main keyword
-        JSONArray jsonArr = jsonObj.getJSONArray(Key);
-        JSONObject jsonObs = jsonArr.getJSONObject(pricecount);
+        /* We have to create our JsonObject through the JSONstring. The JSONstring contains the whole file data.
+           Our SubKeys are inside JSONArrays. On top is our main keyword
+        */
+        JSONObject jsonObs = creatJsonObject(Key);
 
-        //A simple for-loop to get through all Subkeys. --> subKeyCount = Amount of Subkeys
+        // A simple for-loop to get through all SubKeys. --> subKeyCount = Amount of SubKeys
         for(int i=0; i < subKeyCount; i++ ){
             Price.add(jsonObs.getString(SubKey.get(pricecount)));
             pricecount ++;
         }
     }
 
-    //Its a print Method to have a look about all Keys
+    // It's a print Method to have a look about all Keys
     public void printMainKey() {
         for (int i = 0; i < KeyCount; i++){
             System.out.println(i+1 + " " + MainKey.get(i));
         }
     }
 
-    //Its a print Method to have a look about all Subkeys
+    // It's a print Method to have a look about all SubKeys
     public void printSubKey(){
         for (int i = 0; i < subKeyCount; i++){
             System.out.println(i+1 + " " + SubKey.get(i));
         }
     }
 
-    //Its a print Method for a visualization Subkeys + Price in Doller
+    // It's a print Method for a visualization SubKeys + Price in Euro
     public void printSubkeywithPrice(){
         for (int i = 0; i < subKeyCount; i++) {
-            System.out.println(i+1 + " " + SubKey.get(i)+ " " + Price.get(i) + "$");
+            System.out.println(i+1 + " " + SubKey.get(i)+ " " + Price.get(i) + "€");
         }
+    }
+
+    // Methods to clear the Lists
+    public  void clearSubKeylist () { SubKey.clear();}
+    public  void clearPricelist () { Price.clear(); }
+    public void clearMainKeyList () { MainKey.clear(); }
+    public  void ResetPriceCount () { pricecount = 0; }
+    public  void ResetSubKeyCount () { subKeyCount = 0; }
+    public  void ResetKeyCount () { KeyCount = 0; }
+
+    // Method to clear all Lists at Once
+    public  void ClearAllArrayLists (){
+        SubKey.clear();
+        Price.clear();
+        MainKey.clear();
+    }
+
+    public  void ResetCountVariables (){
+        KeyCount = 0;
+        subKeyCount = 0;
+        pricecount = 0;
     }
 
     public static void main(String[] args) {
         MainPage Main = new MainPage();
         ArrayList<String> Key;
-        ArrayList<String> SubKey;
-        ArrayList<String> Price;
 
         Main.loadDataFromJson("Supermarkt.json");
 
@@ -127,14 +155,16 @@ public class MainPage {
         Main.printMainKey();
 
         System.out.println();
-        Main.getsubKeyFromJson(Main.getJSONstring(), Key.get(1));
-        SubKey = Main.getSubKey();
+        Main.getsubKeyFromJson(Key.get(0));
+        //SubKey = Main.getSubKey();
         Main.printSubKey();
 
         System.out.println();
-        Main.getPriceFromsubKeys(Main.getJSONstring(), Key.get(1));
-        Price = Main.getPrice();
+        Main.getPriceFromsubKeys(Key.get(0));
+        //Price = Main.getPrice();
         Main.printSubkeywithPrice();
+
+
 
     }
 
