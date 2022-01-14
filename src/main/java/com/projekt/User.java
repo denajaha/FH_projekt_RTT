0828372a6a7cad73e82b3e16015d8d15670c0b23
4n7oine <a.lanners@outlook.com>
2022-01-14 19:09:22 +0100

    Code kommentiert

--- src/main/java/com/projekt/User.java
+++ src/main/java/com/projekt/User.java
@@ -25,6 +25,9 @@ public class User {
 
  
 
+    //Neuer User erstellen mit privaten Kontruktor
+    // Neuer User wird über nachfolgende Methode erstellt
+    // Kontrollierte erstellung somit sichergestellt
     private User(String firstname, String surname, String username, String password, boolean newUser, String role, int userid) {
 
         users++;
@@ -57,10 +60,13 @@ public class User {
         updateUserDatabase();
     }
 
+    //Methode zum erstellen eines neuen Users
+    // Gibt ein User zurück
     public static User createNewUser(String firstname, String surname, String username, String password, String role){
         return new User(firstname,surname,username,password, true,role,5);
     }
 
+    //Beim Aufrufen der Methode wird der User, der als Parameter übergeben wird aus der Datenbank gelöscht
     public static void deleteUser(User user){
         for(int i=0; i<userlist.size();i++){
             if(userlist.get(i).equals(user)){
@@ -70,34 +76,49 @@ public class User {
         updateUserDatabase();
     }
 
+    //Gibt Liste aller momentanen User zurück
     public static ArrayList<User> getUsers() {
         return userlist;
     }
 
+    //Gibt Vornamen zurück
     public String getFirstname() {
         return firstname;
     }
 
+    //Gibt Familienname zurück
     public String getSurname() {
         return surname;
     }
 
+    //Gibt Username zurück
+    //Achtung, Username ist Hashverschlüsselt
     public String getUsername() {
         return username;
     }
 
+    //Gibt Password zurück
+    //Achtung, Passwort ist Hashverschlüsselt
     public String getPassword() {
         return password;
     }
 
+    //Gibt Rolle zurück
     public String getRole() {
         return role;
     }
 
+
+    //Gibt IntegrityKey zurück
+    //IntegrityKey besteht zur Kontrolle der Rolle, Username und Passwort
+    //3 kritische Merkmale
+    // Wird aus den 3 Kompenten errechnet
     public String getIntegrityKey(){
         return integrityKey;
     }
 
+    //Setter für Vorname
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
     public void setFirstname(String firstname) {
         int index=-1;
         for(int i=0; i<userlist.size();i++){
@@ -116,6 +137,10 @@ public class User {
         userlist.get(index).firstname = firstname;
     }
 
+    //Setter für Familienname
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
+    //Zum update in DB updateMethode aufrufen
+
     public void setSurname(String surname) {
         int index=-1;
         for(int i=0; i<userlist.size();i++){
@@ -133,6 +158,10 @@ public class User {
         userlist.get(index).surname = surname;
         this.surname = surname;
     }
+
+    //Setter für Username
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
+    //Zum update in DB updateMethode aufrufen
 
     public void setUsername(String username) {
         int index=-1;
@@ -153,6 +182,9 @@ public class User {
         setIntegrityKey(calculateIntegrityKey(this.username, this.getPassword()));
     }
 
+    //Setter für Passwort
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
+    //Zum update in DB updateMethode aufrufen
     public void setPassword(String password) {
         int index=-1;
         for(int i=0; i<userlist.size();i++){
@@ -172,6 +204,9 @@ public class User {
         setIntegrityKey(calculateIntegrityKey(this.username, this.password));
     }
 
+    //Setter für integrityKey
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
+    //Zum update in DB updateMethode aufrufen
     private void setIntegrityKey(String integrityKey) {
         this.integrityKey = integrityKey;
         for(int i=0; i<userlist.size();i++){
@@ -181,10 +216,14 @@ public class User {
         }
     }
 
+    //Rechner für integrityKey, gibt String zurück
+    //DB wird nicht automatisch geupdated, wird lediglich in geladener Liste sowie im User selbst aktualisiert
+
     private String calculateIntegrityKey(String username, String password){
         return sha256(sha256(username)+sha256(password)+sha256(this.role));
     }
 
+    //Einfacher vergleich von 2 Strings, ob Hashwert der gleiche ist
     public boolean compareHash(String input, String toCompare){
         if(sha256(input)==sha256(toCompare)){
             return true;
@@ -193,8 +232,9 @@ public class User {
         }
     }
 
+    //Methode zum laden der Daten aus dem json File
+    //json einfach im Ordner abspeichern oder Pfad ändern
     public static void loadDataFromJson(){
-        //Noch nicht fertig
         StringBuilder string = new StringBuilder();
         try {
             FileReader fileReader = new FileReader("users.json");
@@ -228,7 +268,12 @@ public class User {
         userlist = userl;
     }
 
+    /*
     ///** METHODE VON STACKOVERFLOW --- https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha256-in-java
+    ///Methode zum berechnen eines Hash sha256 Werts.
+    // Für Passwörter, Username etc.
+     */
+
     public static String sha256(final String base) {
         try{
             final MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -247,6 +292,8 @@ public class User {
     }
 
 
+    //toString Methode für die Ausgabe auf der Konsole
+    //Zum testen
     @Override
     public String toString() {
         return "User{" +
@@ -259,6 +306,8 @@ public class User {
                 '}';
     }
 
+    //Methode zum schauen ob ein User in der geladenen Liste existiert
+    // ZUm checken in DB, DB laden und dann Methode ausführen
     public boolean checkIfUsernameExists(String username){
         for (int i=0; i<userlist.size();i++){
             if(userlist.get(i).getUsername().equals(username)){
@@ -268,6 +317,8 @@ public class User {
         return false;
     }
 
+    //Aktuell geladene Liste in Datenbank (JSONFILE abspeichern)
+    // Methode einfach aufrufen
     protected static void updateUserDatabase(){
         //Nur ein Versuch
         JSONArray jsonArray = new JSONArray();
@@ -295,6 +346,8 @@ public class User {
         }
     }
 
+    //Wird benötigt für login
+    //Returned User mit den korrespondierenden Daten
     public static final User getUserCredentials(String username, String password){
         User usera = null;
         for (User user : userlist) {
@@ -309,6 +362,8 @@ public class User {
 
     }
 
+    //Wird benötigt für login
+    //DA Username und PW gehashed, checkt methode ob es ein User mit beidem gibt und returned antwort
     public static final boolean checkCredentials(String username, String password){
 
         boolean result = false;
